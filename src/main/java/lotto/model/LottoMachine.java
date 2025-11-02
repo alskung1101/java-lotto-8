8ac522601d327681530fd3aae14f7e9c9479a561
alskung1101 <alskung1101@naver.com>
2025-11-02 23:58:33 +0900

    feat: 구입 금액에 따른 로또 발행 기능 구현 (로또 개수 계산 및 생성)

---
+++ src/main/java/lotto/model/LottoMachine.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import lotto.Lotto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int PRICE_PER_TICKET = 1000;
+
+    public static List<Lotto> createLottos(int purchaseAmount) {
+        int count = purchaseAmount / PRICE_PER_TICKET;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(LottoGenerator.generate()));
+        }
+        return lottos;
+    }
+}
