167a4c977481f27f1f7a103ce315364d1bae1afb
alskung1101 <alskung1101@naver.com>
2025-11-02 23:57:45 +0900

    feat: 로또 번호 자동 생성 기능 구현 (1~45 범위 중복 없는 6개 숫자)

---
+++ src/main/java/lotto/model/LottoGenerator.java
@@ -0,0 +1,15 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoGenerator {
+
+    public static List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
