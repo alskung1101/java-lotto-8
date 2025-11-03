2697ae9ccc5b29aa5941908dd6d390033600e8e0
alskung1101 <alskung1101@naver.com>
2025-11-03 17:30:56 +0900

    feat(controller): 전체 흐름 통합 및 MVC 구조 최종 연결

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import lotto.model.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
 import java.util.List;
 import java.util.Map;
 
@@ -10,14 +11,15 @@ public class LottoController {
 
     public void run() {
         int purchaseAmount = InputView.inputPurchaseAmount();
+
         List<Lotto> lottos = LottoMachine.createLottos(purchaseAmount);
         OutputView.printLottos(lottos);
 
         List<Integer> winningNumbers = InputView.inputWinningNumbers();
         int bonusNumber = InputView.inputBonusNumber(winningNumbers);
 
-        LottoResultCalculator calculator = new LottoResultCalculator(winningNumbers, bonusNumber);
-        Map<String, Integer> result = calculator.calculate(lottos);
+        LottoResultCalculator resultCalculator = new LottoResultCalculator(winningNumbers, bonusNumber);
+        Map<String, Integer> result = resultCalculator.calculate(lottos);
 
         ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount, result);
         double rate = profitCalculator.calculateRate();
