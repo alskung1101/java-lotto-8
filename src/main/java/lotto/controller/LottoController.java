35f0a6c313604c0101976ba9a674bfa3e7ca753c
alskung1101 <alskung1101@naver.com>
2025-11-03 10:09:39 +0900

    feat: 컨트롤러에 당첨 결과 계산 및 출력 통합 (LottoController)

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,10 @@
 package lotto.controller;
 
-import lotto.model.Lotto;
-import lotto.model.LottoMachine;
+import lotto.model.*;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-
 import java.util.List;
+import java.util.Map;
 
 public class LottoController {
 
@@ -13,5 +12,16 @@ public class LottoController {
         int purchaseAmount = InputView.inputPurchaseAmount();
         List<Lotto> lottos = LottoMachine.createLottos(purchaseAmount);
         OutputView.printLottos(lottos);
+
+        List<Integer> winningNumbers = InputView.inputWinningNumbers();
+        int bonusNumber = InputView.inputBonusNumber(winningNumbers);
+
+        LottoResultCalculator calculator = new LottoResultCalculator(winningNumbers, bonusNumber);
+        Map<String, Integer> result = calculator.calculate(lottos);
+
+        ProfitCalculator profitCalculator = new ProfitCalculator(purchaseAmount, result);
+        double rate = profitCalculator.calculateRate();
+
+        OutputView.printResult(result, rate);
     }
 }
